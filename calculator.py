419257d0b6de63e8b509db2c99d38a384651fb26
Manoj Gautam <surfer.manoj@gmail.com>
2015-08-06 07:27:00 +0545

    Added input_field

--- calculator.py
+++ calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env/python
 #Author: @nix1947
+#Description: Open this program using http://www.codeskulptor.org
 
 #import simplegui
 import simplegui
 
 #Defining global variable
 
-store = 12
-operand = 3
+store = 0
+operand = 0
 
 #Defining helper functions, never call helper function from event handler
 
@@ -43,15 +44,21 @@ def subtract():
     store = store - operand
     output()
 
-#Creating the canvas
 
+def input(i):
+	"""Event handler for add_input field"""
+	global operand
+	operand =  float(i)
+        output()
+
+#Creating the canvas
 frame = simplegui.create_frame("Calculator", 200, 200)
 frame.add_button("Print", output, 100)  #Here 100 define the width of buttons
 frame.add_button("swaptext", swap, 100)
 frame.add_button("Add", add, 100)
 frame.add_button("Divide", divide, 100)
 frame.add_button("subtract", subtract, 100)
-
+frame.add_input("Input", enter, 100)
 #Start the frame
 frame.start()
 
